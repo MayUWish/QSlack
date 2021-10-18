42dfdf039bc5b2d006b3ea0a1f5f33660e641348
mqu <meitongdepaul@gmail.com>
2021-10-18 15:50:49 -0700

    test websocket

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
+# import socketio object
+from app.api.test_websocket import socketio
+
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
@@ -33,6 +36,10 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 db.init_app(app)
 Migrate(app, db)
+
+# initialize the app with the socket instance
+# include this line right after Migrate(app, db)
+socketio.init_app(app)
 
 # Application Security
 CORS(app)
@@ -70,3 +77,8 @@ def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
+
+
+# at the bottom of the file, use this to run the app
+if __name__ == '__main__':
+    socketio.run(app)
