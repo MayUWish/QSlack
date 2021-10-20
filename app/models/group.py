14855407833bb2c46b2c0498ee6ab36b3958ff15
mqu <meitongdepaul@gmail.com>
2021-10-20 10:49:13 -0700

    finish db model for user, group, membership, message

---
+++ app/models/group.py
@@ -0,0 +1,42 @@
+from .db import db
+from datetime import datetime
+from .user import memberships
+
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+
+    id = db.Column(db.Integer, primary_key=True)
+    isDM = db.Column(db.Boolean, nullable=False, default=False)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    createdAt = db.Column(db.DateTime, nullable=False,
+                          default=datetime.now())
+    updatedAt = db.Column(db.DateTime, nullable=False,
+                          default=datetime.now())
+    # many to one user
+    adminId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    admin = db.relationship('User', back_populates='groupsOwned')
+
+    # many groups to many users through memberships
+    members = db.relationship(
+        "User", back_populates="groupsJoined", secondary=memberships)
+    
+    # one to many messages
+    messages = db.relationship(
+        'Group', back_populates='group', cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'isDM': self.isDM,
+            'name': self.name,
+            'description': self.description,
+            'createdAt': self.createdAt,
+            "updatedAt": self.updatedAt,
+            "adminId": self.adminId,
+            "admin": self.admin,
+            "members": [member.to_dict() for member in self.members],
+            "messages": [m.to_dict() for m in self.messages],
+        }
