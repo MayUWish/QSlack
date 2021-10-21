f4055130863fc1525f9cb635838cae136a81f8b0
mqu <meitongdepaul@gmail.com>
2021-10-21 14:12:41 -0700

    finish api route for editing chat groups

--- app/api/group_routes.py
+++ app/api/group_routes.py
@@ -24,9 +24,9 @@ def get_groups():
 def create_group():
     form = CreateGroupForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-    print('!!!form before', form.data)
+    # print('!!!form before', form.data)
     if form.validate_on_submit():
-        print('!!!form after', form.data)
+        # print('!!!form after', form.data)
         group = Group(name=form.data['name'],
                       description=form.data['description'],
                       adminId=current_user.id,
@@ -60,4 +60,22 @@ def delete_group(id):
         db.session.commit()
         return {'deletedGroupId': int(id), 'userId': userId}
 
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@group_routes.route('/<int:id>', methods=['PATCH'])
+@login_required
+def create_group(id):
+    form = CreateGroupForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    print('!!!form before', form.data)
+    if form.validate_on_submit():
+        print('!!!form after', form.data)
+        group = Group.query.get(id)
+        if group.adminId != current_user.id:
+            return {'errors': ['No authorization']}, 403
+        group.name = form.data['name']
+        group.description = form.data['description']
+        db.session.commit()
+        return group.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
