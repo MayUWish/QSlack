6926b622ec3931e94093dbaf0a7f0bad61df888a
mqu <meitongdepaul@gmail.com>
2021-10-21 05:19:10 -0700

    display members to each chat group or DM channel

--- react-app/src/components/MainContent/MainContent.js
+++ react-app/src/components/MainContent/MainContent.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import { useSelector} from "react-redux";
+import defaultProfilePic from '../../static/images/defaultProfilePic.png';
+import MembersModal from './MembersModal';
+import './MainContent.css';
 
 function MainContent({groupId}) {
     const chatGroups = useSelector((state) => state.chatGroups);
     const dmChannels = useSelector((state) => state.dmChannels);
+    const currentGroupName = chatGroups[groupId] ? chatGroups[groupId].name : dmChannels[groupId].name
     const messagesArr = chatGroups[groupId] ? chatGroups[groupId].messages : dmChannels[groupId].messages
     const membersObject = chatGroups[groupId] ? chatGroups[groupId].members : dmChannels[groupId].members
+    
 
     console.log('!!!messagesArr>>>', messagesArr)
-
+  
 
     return (
-        <>
+        <>  
+            <div className='chatHeaderWrapper'>
+                <div className='chatHeaderEl'>
+                    <h3 style={{ display: 'inline' }}>{currentGroupName}</h3>
+                </div>
+
+                <div style={{ display: 'flex', width: '95%', justifyContent: 'end' }}>
+                
+                    {/* <div className='chatHeaderEl' style={{ border: '1px solid lightgray', width:'8%'}}
+
+                    >
+                        <i className="fas fa-users" style={{paddingRight: '5px'}}/>
+                        {Object.keys(membersObject).length}
+                    </div> */}
+                    <MembersModal membersObject={membersObject} currentGroupName={currentGroupName}/>
+
+                    <div className='chatHeaderEl'>
+                        <i className="fas fa-user-plus" />
+                    </div>
+
+                    <div className='chatHeaderEl'>
+                        <i className="fas fa-info-circle" />
+                    </div>
+
+                    <div className='chatHeaderEl'>
+                        <i className="fas fa-trash-alt" />
+                    </div>
+
+                </div>
+                
+            </div>
+            
             {messagesArr.map((message,i)=>(
-                <div key={`message${i}`}>
-                    {membersObject[String(message.userId)].username}: {message.message}
+                <div className="eachChatWrapper" key={`message${i}`}>
+                    <img className='chatProfilePic' alt='profilePicture' src={membersObject[String(message.userId)].profilePic ? membersObject[String(message.userId)].profilePic : defaultProfilePic}/>{membersObject[String(message.userId)].username}: {message.message}
                 </div>
             ))}
             <form>
---
+++ react-app/src/components/MainContent/MembersModal/Members.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import defaultProfilePic from '../../../static/images/defaultProfilePic.png';
+
+
+
+const Members = ({ membersObject, currentGroupName }) => {
+    
+    
+
+    return (
+        <div>
+            <h3>{currentGroupName}</h3>
+            {Object.keys(membersObject).map((memberId, i) => (
+                <div className="eachChatWrapper" key={`message${i}`}>
+                    <img className='chatProfilePic' alt='profilePicture' src={membersObject[memberId].profilePic ? membersObject[memberId].profilePic : defaultProfilePic} /> {membersObject[memberId].username}
+                </div>
+            ))}
+            
+        </div>
+    );
+};
+
+export default Members;
