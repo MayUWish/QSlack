7c31ca9ec74b3ff2b8209907bd2edf74da49b29c
mqu <meitongdepaul@gmail.com>
2021-10-30 23:48:24 -0700

    update redux store about users info when clicking on adding people into chat group in case other users update their profile

--- react-app/src/components/AddMemberModal/index.js
+++ react-app/src/components/AddMemberModal/index.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import AddMemberForm from './AddMemberForm';
+import { authenticate } from "../../store/session";
+import { useDispatch } from 'react-redux';
 
 
 function AddMemberModal({ membersObject, currentGroupName, currentGroupId }) {
     const [showModal, setShowModal] = useState(false);
+    const dispatch = useDispatch();
 
     return (
 
         <div>
-            <button className='middleBtn' onClick={() => setShowModal(true)}>               
+            <button className='middleBtn' 
+                    onClick={async() => {
+                        setShowModal(true)
+                        await dispatch(authenticate())
+                        }}>               
                 <i className="fas fa-user-plus fa-2x chatIcon" />
             </button>
 
--- react-app/src/components/CreateGroupModal/index.js
+++ react-app/src/components/CreateGroupModal/index.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import CreateGroupForm from './CreateGroupForm';
+import { authenticate } from "../../store/session";
+import { useDispatch } from 'react-redux';
 
 
 
 function CreateGroupFormModal() {
     const [showModal, setShowModal] = useState(false);
-   
+    const dispatch = useDispatch();
 
     return (
         <div style={{textAlign:'end'}}>
             <button className= 'smallBtn' 
-                    onClick={() => setShowModal(true)}>
+                    onClick={async() => {
+                        setShowModal(true)
+                        await dispatch(authenticate())
+                        }}>
                 <i className="fas fa-plus" />
             </button>
             {showModal && (
